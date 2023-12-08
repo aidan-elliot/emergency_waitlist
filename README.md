a8809cb9db983ea1a69f192e0575bf9396dc06de
Aidan Elliot <aidanelliot@icloud.com>
2023-12-08 12:22:15 -0500

    Made logo bg transparent

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 Required installs
 
-npm install
-
-npm install @fontsource/inter
-
-npm install express
-
-npm install concurrently --save-dev
-
-npm install cors
-
-npm install lodash 
-
-npm install react-router-dom
-
-npm install react-draggable
-
-npm install react-grid-layout
+n
--- src/sidebar.js
+++ src/sidebar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faUserNurse, faEnvelope, faBars } from '@fortawesome/free-solid-svg-icons';
-import logo from './HospitalApplogo.png'
+import logo from './HospitalApplogo-removebg-preview.png'
 
 function Sidebar(props) {
 
