886c77070188f0236481adbe03273e5fdc727c08
Aidan Elliot <aidanelliot@icloud.com>
2023-12-10 22:04:48 -0500

    file naming fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Sidebar from './sidebar';
+import Sidebar from './Sidebar';
 import Dashboard from './dashboard';
 
 function App() {
